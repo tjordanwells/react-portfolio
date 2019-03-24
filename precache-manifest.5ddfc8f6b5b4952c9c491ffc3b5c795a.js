07bc42b5f2b5fe141c81238517b5f735ebb85e94
Jordan <tjordanwells@gmail.com>
2019-03-23 22:56:39 -0400

    Updates

---
+++ precache-manifest.5ddfc8f6b5b4952c9c491ffc3b5c795a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "da284a2715c20101ab4aea0b06dcffac",
+    "url": "/static/media/wikiwalk.da284a27.png"
+  },
+  {
+    "revision": "369241a4a72d84b4fbaf17de5f4d9a75",
+    "url": "/static/media/rickandmorty.369241a4.jpg"
+  },
+  {
+    "revision": "8a64493baf827f201e1148476cb891b3",
+    "url": "/static/media/nyt.8a64493b.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "01b6473338812bb2b305",
+    "url": "/static/js/main.01b64733.chunk.js"
+  },
+  {
+    "revision": "e08a43e0e716bfd7206a",
+    "url": "/static/js/1.e08a43e0.chunk.js"
+  },
+  {
+    "revision": "9d206d4422205e2d1691119a14cf7242",
+    "url": "/index.html"
+  }
+];
