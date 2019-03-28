075c3ceab0211d25c0278be1112cccea5e4de211
Jordan <tjordanwells@gmail.com>
2019-03-28 08:58:48 -0400

    finished form | adding png for social navbar

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,39 +1,152 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
+import React, { Component } from 'react';
+import { Grid, TextField, Paper, Typography, Button } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
 import * as emailjs from 'emailjs-com';
 
-const styles = () => ({
+const styles = theme => ({
   root: {
-    flexGrow: 1,
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 4,
+    paddingBottom: theme.spacing.unit * 6,
+    marginTop: theme.spacing.unit * 4
+  },
+  form: {
+    justifyContent: 'center'
+  },
+  lower: {
+    marginTop: theme.spacing.unit * 6,
+    justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    fontWeight: 500
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+  button: {
+    margin: theme.spacing.unit,
   }
 });
 
-const formSubmit = () => { emailjs.sendForm(process.env.EJS_SERVICE_ID, process.env.EJS_TEMPLATE_ID, '#myForm', process.env.EJS_USER_ID)
+class Contact extends Component {
+
+  state = {
+      name: '',
+      subject: '',
+      email: '',
+      message: ''
+    };
+  
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  formSubmit = () => { emailjs.send('gmail', 'template_wXRGbnfO', this.state, 'user_hF2KvSHVU1Y2sHgYSC7Q5')
     .then((response) => {
-       console.log('SUCCESS!', response.status, response.text);
+       console.log('SUCCESS!', response.status, response.text, this.state);
     }, (err) => {
        console.log('FAILED...', err);
     });
-};
+  };
 
-console.log(process.env.EJS_SERVICE_ID)
-
-const Contact = (props) => {
-  const { classes } = props;
+  render() {
+    const { classes } = this.props;
     return (
       <div className={classes.root}>
-        <Grid container spacing={24}>
-          <Grid item md={6}>
-            <p>
-                contact form?
-            </p>
+        <Grid className={classes.form} container spacing={24}>
+          <Grid item lg={12}>
+            <Typography className={classes.text} component="h2" variant="h2" gutterBottom>
+              Interested in working together?
+            </Typography>
           </Grid>
+          <Grid item lg={12}>
+            <Typography className={classes.text} component='h6' variant='h6' gutterBottom>
+              I'm always on the lookout for new projects.
+            </Typography>
+          </Grid>
+          <Paper className={classes.root} elevation={1}>
+            <form className={classes.container} noValidate autoComplete="off" id='contactForm'>
+              <Grid item xs={5}>
+                <TextField
+                  id="outlined-name"
+                  label="Name"
+                  className={classes.textField}
+                  value={this.state.name}
+                  onChange={this.handleChange('name')}
+                  margin="normal"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid item xs={1} />
+              <Grid item xs={6}>
+                <TextField
+                  id="outlined-email"
+                  label="Email Address"
+                  className={classes.textField}
+                  value={this.state.email}
+                  onChange={this.handleChange('email')}
+                  margin="normal"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <TextField
+                  id="outlined-subject"
+                  label="Subject"
+                  className={classes.textField}
+                  value={this.state.subject}
+                  onChange={this.handleChange('subject')}
+                  margin="normal"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid item xs={8} />
+              <Grid item xs={12}>
+                <TextField
+                  id="outlined-multiline-flexible"
+                  label="Message"
+                  multiline
+                  rowsMax="4"
+                  value={this.state.message}
+                  onChange={this.handleChange('message')}
+                  className={classes.textField}
+                  margin="normal"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+            </form>
+          </Paper>
+        </Grid>
+        <Grid className={classes.lower} container spacing={24}>
+          <Grid item xs={5} />
+            <Grid item xs={2}>
+              <Button variant="outlined" color="inherit" className={classes.button} fullWidth onClick={this.formSubmit}>
+                Send
+              </Button>
+            </Grid>
+          <Grid item xs={5} />
         </Grid>
       </div>
     )
+  }
 }
 
 Contact.propTypes = {
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import  { Grid, Grow } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
@@ -9,7 +9,7 @@ import wikiwalk from '../../images/wikiwalk.png';
 import nyt from '../../images/nyt.png';
 import rickandmorty from '../../images/rickandmorty.jpg';
 
-const styles = theme => ({
+const styles = () => ({
   root: {
     flexGrow: 1,
   },
@@ -20,66 +20,33 @@ const styles = theme => ({
     width: '100%',
     top: 0,
     justifyContent: 'center',
-    margin: theme.spacing.unit
   }
 });
 
-class Portfolio extends Component {
-
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      willGrow: false
-    }
-  };
-
-  componentDidMount() {
-    this.setState({
-      willGrow: true
-    });
-    console.log(this.state.willGrow)
-  };
-
-  render() {
-    const { classes } = this.props;
-
-    const wikiProj = (
-      <Project
-        title='Wiki Walking Tours'
-        projectImage={wikiwalk}
-        description='With Wiki Walking Tours you can easily find Wikipedia articles of cool places near you.'
-        codeUrl='https://github.com/awb305/wikiwalk'
-        demoUrl='https://polar-reaches-51925.herokuapp.com/'
-      />
-    );
-
-    const nytProj = (
-      <Project
-        title='NYT Web Scraper'
-        projectImage={nyt}
-        description='Simple web-scraper that displays recent articles from The New York Times.'
-        codeUrl='https://github.com/tjordanwells/nyt-mongo-scrape'
-        demoUrl='https://sheltered-cove-94630.herokuapp.com/'
-      />
-    );
-
+const Portfolio = (props) => {
+  const { classes } = props;
     return (
       <div className={classes.root}>
         <Grid className={classes.portfolio} container spacing={24}>
           <Grid item>
             <Grow in={true}>
-              {wikiProj}
+              <Project
+                title='Wiki Walking Tours'
+                projectImage={wikiwalk}
+                description='With Wiki Walking Tours you can easily find Wikipedia articles of cool places near you.'
+                codeUrl='https://github.com/awb305/wikiwalk'
+                demoUrl='https://polar-reaches-51925.herokuapp.com/'
+              />
             </Grow>
           </Grid>
           <Grid item>
-          <Grow
-            in={true}
-            style={{ transformOrigin: '0 0 0' }}
-            {...(true ? { timeout: 1000 } : {})}
-          >
-            {nytProj}
-          </Grow>
+            <Project
+              title='NYT Web Scraper'
+              projectImage={nyt}
+              description='Simple web-scraper that displays recent articles from The New York Times.'
+              codeUrl='https://github.com/tjordanwells/nyt-mongo-scrape'
+              demoUrl='https://sheltered-cove-94630.herokuapp.com/'
+            />
           </Grid>
           <Grid item>
             <Project
@@ -93,7 +60,6 @@ class Portfolio extends Component {
         </Grid>
       </div>
     )
-  }
 }
 
 Portfolio.propTypes = {
