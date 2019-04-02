0bf9009fc434abdb00c4c237f83ae2bdd770c073
Jordan <tjordanwells@gmail.com>
2019-04-01 23:43:40 -0400

    pointing / to about

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
       <Router>
         <Nav />
         <Switch>
-          <Route exact path='/' component={Home} />
+          <Route exact path='/' component={About} />
           <Route exact path='/about' component={About} />
           <Route exact path='/contact' component={Contact} />
           <Route exact path='/portfolio' component={Portfolio} />
